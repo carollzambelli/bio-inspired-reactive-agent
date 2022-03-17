4d0c4959867f0e82b5ed1a4dbda47bf731d7e06b
carollzambelli <caroll.zambelli@gmail.com>
2022-03-17 19:24:47 -0300

    minds json

--- scripts/agent.py
+++ scripts/agent.py
@@ -59,7 +59,22 @@ class Agent:
         
         return msg, iNext, possible_moves, flgReward
 
-
+   
+    def agent_action(self, around_map, iAct, memory, flgPossuiReward):
+        
+        agents_catalog = {
+            1: self.dummy_agent,
+            2: self.coward_agent,
+            3: self.ortogonal_sense_agent
+            #4: self.diag_sense_agent
+        }
+        
+        agent_id = self.experimento['agent']
+        
+        return agents_catalog[agent_id](around_map, iAct, memory, flgPossuiReward)
+    
+    
+    '''
     def diag_sense_agent(self, around_map, iAct, memory, flgReward):
         
         around_map = around_map[1:]
@@ -83,17 +98,36 @@ class Agent:
         msg = self.configs["comando"][str(iNext)]  
         
         return msg, iNext, memory, flgReward
+        
+        
+    def ortogonal_sense_agent(self, around_map, iAct, memory, flgReward):
+        
+        around_map = around_map[1:]
+        explore_map = utils.explore(
+            around_map[:self.experimento["total_calls"]],
+            self.experimento,
+            self.experimento["total_calls"])
+        
+                        
+        if iAct != None:
+            behind = self.configs['behind'][str(iAct)]
+            explore_map[behind] = [1,0] #força o agente a não voltar de onde veio
+            
+        print(explore_map)
+
+        if self.experimento["mind"]["goal"] in explore_map:
+            possible_moves = [
+                i for i, j in enumerate(explore_map) if j == self.experimento["mind"]["goal"]]   
+            flgReward = True  
+        elif self.experimento["mind"]["ok"]  in explore_map:
+            possible_moves = [
+                i for i, j in enumerate(explore_map) if j == self.experimento["mind"]["ok"]]
+        else:
+            possible_moves = [behind]
+        
+        iNext = random.choice(possible_moves)
+        msg = self.configs["comando"][str(iNext)]  
+        
+        return msg, iNext, possible_moves, flgReward
     
-    
-    def agent_action(self, around_map, iAct, memory, flgPossuiReward):
-        
-        agents_catalog = {
-            1: self.dummy_agent,
-            2: self.coward_agent,
-            3: self.ortogonal_sense_agent,
-            4: self.diag_sense_agent
-        }
-        
-        agent_id = self.experimento['agent']
-        
-        return agents_catalog[agent_id](around_map, iAct, memory, flgPossuiReward)
+    ''' 
